b7becfbb4a27747c2aaca5ddfb40a4c4b09866cd
vlad1894 <constantinescu.vlad.adrian@gmail.com>
2025-12-07 18:23:11 +0000

    fix urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from . import views
-from .views import add_to_cart, subscribe_newsletter, cart_view
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
     path('about/', views.about, name='about'),
-    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('subscribe/', subscribe_newsletter, name='subscribe'),
-    path('cart/', cart_view, name = 'cart'),
-    path('blog/', views.blog, name = 'blog')
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('subscribe/', views.subscribe_newsletter, name='subscribe'),
+    path('cart/', views.cart_view, name='cart'),
+    path('blog/', views.blog, name='blog'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
